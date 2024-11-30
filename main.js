faaee577a6f013c7fbd46d53b5088b8aec39c0c9
Gustave Boussard <gustave.boussard@gmail.com>
2024-11-30 14:39:05 +0100

    fix: removed raycaster

--- main.js
+++ main.js
@@ -55,6 +55,8 @@ let reticle;
 let hitTestSource = null; // ce qui décla
 let hitTestSourceRequested = false;
 
+let raycaster;
+
 init();
 
 function init() {
@@ -73,6 +75,17 @@ function init() {
   const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 3);
   light.position.set(0.5, 1, 0.25);
   scene.add(light);
+
+  // cubes
+  var cubeGeo = new THREE.BoxGeometry(1, 1, 1, 10, 10);
+  var cubeMaterial = new THREE.MeshPhongMaterial({ color: 0x888888 });
+  for (var i = 0; i < N; i++) {
+    cubeMesh = new THREE.Mesh(cubeGeo, organMaterial);
+    cubeMesh.castShadow = true;
+    meshes.push(cubeMesh);
+    scene.add(cubeMesh);
+  }
+
 
   //
 
